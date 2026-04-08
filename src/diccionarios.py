4fa87e1d05358e27f330988afbaf906d63fe5dc4
Cristian Acho <cbacristian00@gmail.com>
2026-04-07 22:16:07 -0300

    feat: implementacion de funciones del archivo diccionarios.py

--- src/diccionarios.py
+++ src/diccionarios.py
@@ -9,7 +9,11 @@ def contar_palabras(texto: str) -> dict:
     Ejemplo: contar_palabras("hola mundo hola") -> {"hola": 2, "mundo": 1}
     Las palabras deben ser comparadas en minúsculas.
     """
-    # TU CÓDIGO AQUÍ
+    palabras = texto.lower().split()
+    frecuencia = {}
+    for palabra in palabras:
+        frecuencia[palabra] = frecuencia.get(palabra, 0) + 1
+    return frecuencia
     pass
 
 
@@ -18,7 +22,7 @@ def invertir_diccionario(d: dict) -> dict:
     Retorna un nuevo diccionario con claves y valores intercambiados.
     Ejemplo: invertir_diccionario({"a": 1}) -> {1: "a"}
     """
-    # TU CÓDIGO AQUÍ
+    return {valor: clave for clave, valor in d.items()}
     pass
 
 
@@ -26,7 +30,9 @@ def merge_diccionarios(d1: dict, d2: dict) -> dict:
     """
     Combina dos diccionarios. Si hay claves repetidas, prevalece d2.
     """
-    # TU CÓDIGO AQUÍ
+    resultado = d1.copy()
+    resultado.update(d2)
+    return resultado
     pass
 
 
@@ -35,5 +41,5 @@ def filtrar_por_valor(d: dict, minimo: int) -> dict:
     Retorna un nuevo diccionario con solo los pares
     cuyo valor sea >= minimo.
     """
-    # TU CÓDIGO AQUÍ
+    return {clave: valor for clave, valor in d.items() if valor >= minimo}
     pass
